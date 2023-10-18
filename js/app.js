8d8ba50bec2a15f0c1a88b98fae32cf054fcac56
cesu19 <cesarhernancorbalan19@gmail.com>
2023-10-17 21:21:08 -0300

    Cree función para imprimir resultados en el HTML

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ function mostrarAlerta (mensaje) {
 
 function buscarImagenes(termino) {
         const key = '40005983-6fff598da19d72373677aa55f';
-        const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+        const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
         
     fetch (url)
         .then( respuesta => respuesta.json())
@@ -50,5 +50,32 @@ function buscarImagenes(termino) {
 
     function mostrarImagenes(imagenes){
         console.log(imagenes);
+        while(resultado.firstElementChild){
+            resultado.removeChild(resultado.firstChild);
+        }
+        // Iterar sobre Imagenes y construir HTML
+        imagenes.forEach( imagen =>{
+            const { previewURL, likes, views, largeImageURL } = imagen;
+
+            resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                     <p class="font-bold"> ${likes} <span class="font-light"> Me Gusta</span> </p>
+                     <p class="font-bold"> ${views} <span class="font-light"> Visualizaciones</span> </p> 
+                     <a 
+                        class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                        href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
+                        Ver Imagen
+                     </a>
+                    </div>
+
+                </div>
+             </div>
+            `
+        })
+
     }
     
